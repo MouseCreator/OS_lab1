5d55af5948a5c611981c1589f2745fefef31ba82
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-10-06 03:53:17 +0300

    added calculation class

---
+++ src/main/java/org/example/main/completable/calculation/Calculation.java
@@ -0,0 +1,60 @@
+package org.example.main.completable.calculation;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class Calculation<T> {
+    private final CompletableFuture<T> future;
+    private final String name;
+    private String error = "";
+    private final Object sync = new Object();
+    public Calculation(CompletableFuture<T> future, String name) {
+        this.future = future;
+        this.name = name;
+    }
+
+    public String printState() {
+        Future.State state = future.state();
+        return switch (state) {
+            case RUNNING -> name + " is in process";
+            case FAILED -> name + " has failed";
+            case SUCCESS -> name + " is finished";
+            case CANCELLED -> name + " is cancelled";
+        };
+    }
+
+    public Future.State state() {
+        return future.state();
+    }
+
+    public void fail(String message) {
+        error = message;
+        future.completeExceptionally(new Throwable(message));
+    }
+
+    public String getErrorMessage() {
+        return error;
+    }
+    public void cancel() {
+        future.cancel(true);
+    }
+    public T get() throws ExecutionException, InterruptedException {
+        return future.get();
+    }
+
+    public T waitAndGet() throws ExecutionException, InterruptedException {
+        synchronized (sync) {
+            while (!future.isDone()) {
+                sync.wait();
+            }
+            return future.get();
+        }
+    }
+    public void finish() {
+        assert future.isDone();
+        synchronized (sync) {
+            sync.notifyAll();
+        }
+    }
+}
