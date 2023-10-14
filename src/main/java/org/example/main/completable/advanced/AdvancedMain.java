e1b26463176dec1b0896994e920b886bbb08b9e6
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-10-15 01:45:36 +0300

    added todo

--- src/main/java/org/example/main/completable/advanced/AdvancedMain.java
+++ src/main/java/org/example/main/completable/advanced/AdvancedMain.java
@@ -1,6 +1,13 @@
 package org.example.main.completable.advanced;
 
 public class AdvancedMain {
+
+    /*
+    TODO:
+    1. Add locks controller
+    2. Implement status handling
+    1. Fix memoization map
+     */
     public static void main(String[] args) {
         AdvancedController controller = new AdvancedController();
         controller.start();
